f1ed587191b93ccb1c08a11235169f1c82ff19c4
AppsWebSolution <debkanta2019@outlook.com>
2021-03-27 11:50:51 +0530

    Updates

--- precache-manifest.08f0fdd92ced752438473256759ed3ef.js
+++ precache-manifest.08f0fdd92ced752438473256759ed3ef.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a67a9082e6200f0a97eff69a2bf6e4e",
+    "revision": "2ba5320f0440cd168e98cb0af6c8bfcc",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "0c7f6d26cd9d5f2eeb43",
-    "url": "/static/css/main.6ed2a415.chunk.css"
+    "revision": "6657b1a555ea68f279ea",
+    "url": "/static/css/main.0625d18a.chunk.css"
   },
   {
     "revision": "2be63d84c606423c543d",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2a0af3bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0c7f6d26cd9d5f2eeb43",
+    "revision": "6657b1a555ea68f279ea",
     "url": "/static/js/main.161eacf6.chunk.js"
   },
   {
